fc73e536e3a06aa1e271cf2681baae9286a55e04
Thwipshot <ethumpert@gmail.com>
2016-05-16 20:00:47 -0500

    coded the canvas path to draw the function

--- linechart.js
+++ linechart.js
@@ -7,14 +7,25 @@ if (this.FunWithCanvas === undefined) this.FunWithCanvas = {};
 
     var theCanvas = document.querySelector('#the-canvas');
     var context = theCanvas.getContext('2d');
+    var x = 0;
+    var height = theCanvas.clientHeight;
+    var width = theCanvas.clientWidth;
 
+    console.log(height, width);
     context.strokeStyle = '#00F';
 
     var data = [30, 50, 90, 87, 75, 120, 47, 15, 120, 65, 60];
     context.beginPath();
-    context.strokeStyle= 'green';
     //define the path
+    context.moveTo(x, height-data[0]);
+    for (var i= 1; i < data.length; i++) {
 
+      x += 50;
+      context.lineTo(x, height-data[i]);
+    }
+    context.lineTo(width,height);
+    context.lineTo(0, height);
+    context.closePath();
     context.stroke();
 
   }
